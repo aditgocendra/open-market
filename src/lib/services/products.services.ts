a04f494e87063c49c1301bae69fa14bbe4cc5876
aditgocendra <gocendra123@gmail.com>
2024-09-28 13:36:44 +0700

    add prouct info

--- src/lib/services/products.services.ts
+++ src/lib/services/products.services.ts
@@ -1,4 +1,5 @@
 import prisma from "../../lib/prisma.init";
+import { ProductInfoDto } from "../dto/product.dto";
 
 export const getProductsService = async ({
   idCategory,
@@ -28,11 +29,11 @@ export const getProductsService = async ({
         id: true,
         name: true,
         price: true,
-        ProductImages: { select: { image: true }, where: { isThumb: true } },
+        productImages: { select: { image: true }, where: { isThumb: true } },
       },
       orderBy: {
         ...(filter === "latest" ? { createdAt: "desc" } : {}),
-        DetailProduct: {
+        detailProduct: {
           ...(filter === "bestSeller" ? { totalSold: "desc" } : {}),
           ...(filter === "favorite" ? { totalFav: "desc" } : {}),
           ...(filter === "trending" ? { seen: "desc" } : {}),
@@ -41,5 +42,56 @@ export const getProductsService = async ({
     });
   } catch (error) {
     throw new Error("Failed to fetch category");
+  }
+};
+
+export const getProductInfoService = async (id: string) => {
+  try {
+    const r = await prisma.product.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        name: true,
+        price: true,
+        category: { select: { name: true } },
+        nameSubCategory: true,
+        detailProduct: true,
+        productImages: true,
+        variantProduct: true,
+        store: true,
+      },
+    });
+
+    const productInfo: ProductInfoDto = {
+      name: r?.name || "",
+      category: r?.category?.name || "",
+      subCategory: r?.nameSubCategory || "",
+      desc: r?.detailProduct?.description || "",
+      price: r?.price || 0,
+      rating: r?.detailProduct?.rating || 0.0,
+      seen: r?.detailProduct?.seen || 0,
+      totalFav: r?.detailProduct?.totalFav || 0,
+      totalStock: r?.detailProduct?.totalStock || 0,
+      totalSold: r?.detailProduct?.totalSold || 0,
+      images: r?.productImages?.map((p) => p.image) || [],
+      variant:
+        r?.variantProduct.map((v) => {
+          return {
+            id: v.id,
+            variant: v.variant,
+            price: v.price,
+            stock: v.stock,
+            image: v.image || null,
+          };
+        }) || [],
+      store: {
+        name: r?.store?.name || "",
+        logo: r?.store?.logo || null,
+      },
+    };
+
+    return productInfo;
+  } catch (error) {
+    throw new Error("Failed to fetch product");
   }
 };
