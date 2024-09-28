79039673f788528441bddd5b0f1cf1db3edc62be
aditgocendra <gocendra123@gmail.com>
2024-09-28 13:41:06 +0700

    remove style flex

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body>
-        <div className='w-full h-screen flex flex-col'>
+        <div className='w-full h-screen'>
           <NavbarComponent />
           {children}
           <FooterComponent />
