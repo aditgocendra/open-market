7d88b4460592be46055a9173ae5d4da162bde827
aditgocendra <gocendra123@gmail.com>
2024-10-01 20:08:22 +0700

    add ButtonTextComponent

--- src/components/button-component.tsx
+++ src/components/button-component.tsx
@@ -1,3 +1,5 @@
+import { useFormStatus } from "react-dom";
+
 export const ButtonTextComponent = ({
   text,
   isActive,
@@ -14,6 +16,19 @@ export const ButtonTextComponent = ({
       }`}
       onClick={() => onClickButton && onClickButton()}>
       {text}
+    </button>
+  );
+};
+
+export const ButtonSubmitForm = ({ title }: { title: string }) => {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      disabled={pending}
+      type='submit'
+      className='flex w-full justify-center bg-slate-800 text-white text-sm rounded-md px-4 py-2 hover:bg-slate-700'>
+      {pending ? "Loading..." : title}
     </button>
   );
 };
