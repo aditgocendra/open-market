ecc997a1f71a8d66870cefefb29db808e08f1744
aditgocendra <gocendra123@gmail.com>
2024-10-01 10:31:48 +0700

    add getProducts

--- src/app/(root)/actions.ts
+++ src/app/(root)/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { getCategoriesService } from "@/lib/services/categories.services";
+import { getProductsService } from "@/lib/services/products.services";
 
 export const getCategoriesAction = async ({
   take,
@@ -13,6 +14,21 @@ export const getCategoriesAction = async ({
     const r = await getCategoriesService({ take, skip });
 
     return { data: r.data, count: r.count };
+  } catch (error: unknown) {
+    return { errorMessage: (error as Error).message };
+  }
+};
+
+export const getProductsAction = async ({
+  take,
+  skip,
+}: {
+  take: number;
+  skip: number;
+}) => {
+  try {
+    const r = await getProductsService({ take, skip });
+    return { data: r.products, count: r.count };
   } catch (error: unknown) {
     return { errorMessage: (error as Error).message };
   }
