8cdd79caad046c026be81050d9e336bb97fc3bea
aditgocendra <gocendra123@gmail.com>
2024-10-24 13:45:43 +0700

    add action addProductToCartAction

--- src/app/(root)/product/[slug]/actions.ts
+++ src/app/(root)/product/[slug]/actions.ts
@@ -1,5 +1,29 @@
+"use server";
+
+import { decrypt } from "@/lib/jwt";
+import { createCartService } from "@/lib/services/cart.services";
 import { getProductInfoService } from "@/lib/services/products.services";
+import { revalidatePath } from "next/cache";
+import { cookies } from "next/headers";
 
 export const getProductInfoAction = async (id: string) => {
   return await getProductInfoService(id);
+};
+
+export const addProductToCartAction = async ({
+  productId,
+  qty,
+  variantId,
+}: {
+  productId: string;
+  qty: number;
+  variantId?: number;
+}) => {
+  const cookie = cookies().get("session")?.value;
+  const session = await decrypt(cookie!);
+
+  const uid = session?.userId as string;
+
+  await createCartService({ uid, productId, variantId, qty });
+  revalidatePath("/cart");
 };
