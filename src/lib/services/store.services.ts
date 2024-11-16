e69b381681e96dbc629fc0f79f3164e7ec902ae4
aditgocendra <gocendra123@gmail.com>
2024-11-16 12:44:56 +0700

    add get & set information service

--- src/lib/services/store.services.ts
+++ src/lib/services/store.services.ts
@@ -24,12 +24,70 @@ export const getStoreByIdService = async (id: string) => {
       id: r?.id || "",
       name: r?.name || "",
       logo: r?.logo || "",
-      regencies: r?.location.regencies || "",
+      regencies: r?.location!.regencies || "",
       createdAt: r?.createdAt || new Date(),
     };
 
     return storeDetail;
   } catch (error) {
     throw new Error("Something went wrong");
+  }
+};
+
+export const getMyStoreInformationService = async (uid: string) => {
+  try {
+    return await prisma.store.findUnique({
+      where: {
+        userId: uid,
+      },
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        logo: true,
+      },
+    });
+  } catch (error) {
+    throw new Error("Something went wrong");
+  }
+};
+
+export const setStoreInformationService = async ({
+  uid,
+  data,
+}: {
+  uid: string;
+  data: any;
+}) => {
+  try {
+    return await prisma.store.upsert({
+      where: {
+        userId: uid,
+      },
+      create: {
+        userId: uid,
+        ...data,
+      },
+      update: {
+        ...data,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    throw new Error("Something went wrong");
+  }
+};
+
+export const updateStoreService = async ({
+  uid,
+  data,
+}: {
+  uid: string;
+  data: any;
+}) => {
+  try {
+    return await prisma.store.update({ where: { userId: uid }, data });
+  } catch (error) {
+    throw new Error("Something wrong, please wait a seconds");
   }
 };
